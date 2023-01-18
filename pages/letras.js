5ff71d999700181a422930079856ef4747937608
Daniel <claxon_21@hotmail.com>
2023-01-18 01:34:11 -0600

    Agrega Layout/Navbar de alumnos, componentes de Letras/palabras

--- pages/letras.js
+++ pages/letras.js
@@ -1,8 +1,6 @@
-import Image from 'next/image';
 import React, {useState} from 'react';
 import AlumnoLayout from '../components/AlumnoLayout';
 import Abecedario from '../components/Letras/ABecedario'
-import Carrusel from '../components/Letras/Carrusel';
 
 
 const Letras = () => {
@@ -25,11 +23,6 @@ const Letras = () => {
                                 }
                         }/> 
                         <h3 className='w-full flex sm:justify-center items-center'>Imagen</h3>
-                        {/* <Image 
-                            src={'https://i.ibb.co/zxXFRHB/Fc-Oy-Vh-HWQAA-f-U.jpg'}
-                            width={300}
-                            height={300}
-                        /> */}
                         
                     </div>
                 </div>
