bf75d3698968ae7b80bfce72c92bfbec97fffe24
Daniel <claxon_21@hotmail.com>
2023-01-18 00:54:18 -0600

    Agrega Layout/Navbar de alumnos, componentes de Letras/palabras

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
         <title>Apprende APP</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
         <script src="https://cdn.tailwindcss.com"></script>
-        <script src="./TW-ELEMENTS-PATH/dist/js/index.min.js"></script>
+        
       </Head>
       <Component {...pageProps} />
     </ApolloProvider>
